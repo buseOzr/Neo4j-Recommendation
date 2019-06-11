251d670b5562afb02e31d2452ccfe19e8476b40a
buseOzr <buse.ozer@est.fib.upc.edu>
2019-06-11 19:51:32 +0200

    main

---
+++ main.py
@@ -0,0 +1,98 @@
+from py2neo import Graph, Node, Relationship, NodeMatcher
+import os
+import csv
+
+url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+graph = Graph(url + '/db/data', username='neo4j', password='bdma')
+print("connected")
+
+
+def populateTasker():
+    with open('taskers.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for line in csv_reader:
+            username = line['username']
+            passportNumber = line['passportNumber']  # take the current line
+            profession = line['profession']
+            region = line['region']
+            user = Node("Tasker", username=username, passportNumber=passportNumber, profession=profession,
+                        region=region)
+            graph.create(user)
+            print("done")
+
+
+# populateTasker()
+
+def populateCustomer():
+    with open('Customers2.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for line in csv_reader:
+            username = line['username']
+            passportNumber = line['passportNumber']  # take the current line
+            user = Node("Customer", username=username, passportNumber=passportNumber)
+            graph.create(user)
+            print("done")
+
+
+def findCustomer():
+    value = 'amays'
+    matcher = NodeMatcher(graph)
+    user = matcher.match("Customer", username="amays").first()
+    # user =graph.match("Tasker", 'username', value).first()
+    print(user)
+
+
+def find_similarities():
+    query = '''
+    MATCH (p:Customer), (m:Tasker) 
+    OPTIONAL MATCH (p)-[rated:booked]->(m) 
+    WITH {item:id(p), weights: collect(coalesce(toInt(rated.priceRating),toInt(rated.qualityRating), toInt(rated. flexScheduleRating)))} as userData 
+    WITH collect(userData) as data 
+    CALL algo.similarity.pearson.stream(data,{topK:7,similarityCutoff:0.2}) 
+    YIELD item1, item2, count1, count2, similarity 
+    WITH algo.asNode(item1).username AS from, algo.asNode(item2).username AS to, similarity 
+    match(ff:Customer{username:from}) 
+    match(tt:Customer{username:to}) 
+    MERGE (ff)-[sim:SIMILAR]->(tt) 
+    SET sim.similarity=similarity 
+    '''
+    return graph.run(query)
+
+
+def update_weight1():
+    query = '''
+    OPTIONAL MATCH (c:Customer)-[b:booked]->(t:Tasker) 
+    CREATE (c)-[w:WEIGHT]->(t) 
+    SET w.value=(1/ToFloat(b.overallRating)) 
+    '''
+    return graph.run(query)
+
+
+def update_weight2():
+    query = '''
+    OPTIONAL MATCH (c1:Customer)-[s:SIMILAR]->(c2:Customer) 
+    CREATE (c1)-[w:WEIGHT]->(c2) 
+    SET w.value=1-ToFloat(s.similarity) 
+    '''
+    return graph.run(query)
+
+
+def calculate_shortestpath():
+    query = '''
+    MATCH (start: Customer {username:'afisher'}) 
+    CALL algo.shortestPaths.stream(start, 'value',{ 
+    nodeQuery:'MATCH(n: Customer) RETURN id(n) as id UNION MATCH(m:Tasker) RETURN id(m) as id', 
+    relationshipQuery:'MATCH(n)-[r:WEIGHT]-(m) RETURN id(n) as source, id(m) as target, r.value as weight',direction:'both', 
+    graph:'cypher'}) 
+    YIELD nodeId, distance 
+    WITH algo.asNode(nodeId) AS Node, distance 
+    WHERE algo.isFinite(distance) = true AND 'Tasker' IN LABELS(Node) 
+    RETURN Node.username, distance 
+    ORDER BY distance DESC 
+    find_similarities()
+'''
+    return graph.run(query)
+
+# findCustomer()
